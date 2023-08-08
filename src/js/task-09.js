a75768e29fee52562db11eef81415505a9b2034e
0leksandrompm <101967309+0leksandrompm@users.noreply.github.com>
2023-08-08 14:43:37 +0300

    9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+console.log(getRandomHexColor())
+
+const bgColor = document.querySelector('.color');
+const changeBg = document.querySelector('.change-color')
+const vidg= document.querySelector('.widget');
+
+
+
+changeBg.addEventListener('click', () => {
+  bgColor.textContent = getRandomHexColor();
+  vidg.parentNode.style.backgroundColor = getRandomHexColor();
+})
