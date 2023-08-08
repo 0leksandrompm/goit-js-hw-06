ddf70fec1092936154cd3cd7cc763354759d9116
0leksandrompm <101967309+0leksandrompm@users.noreply.github.com>
2023-08-08 15:52:23 +0300

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,23 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const contrBox =document.querySelector("#controls")
+const boxBox =document.querySelector("#boxes ")
+const divka = document.createElement("div");
+
+contrBox.addEventListener("submit", createBoxes);
+
+function createBoxes(amount){
+  const quantity=[];
+
+  const { elements: { email, password }} = evt.currentTarget;
+
+  for (let i=0; 1<amount; i+=1){
+    if (Number(amount)>=1){
+      quantity.push(amount(i))
+    }
+  }
+  console.log(quantity)
+  boxBox.append(...quantity)
+}
